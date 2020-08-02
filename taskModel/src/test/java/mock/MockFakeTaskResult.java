661d8e6af2318eabafe890954134614925f39478
Volodymyr (or Vladimir) Blagodarov <maestrov13@gmail.com>
2020-08-02 20:39:45 +0200

    fix mockTestResult impl

--- taskModel/src/test/java/mock/MockFakeTaskResult.java
+++ taskModel/src/test/java/mock/MockFakeTaskResult.java
@@ -15,11 +15,11 @@ public class MockFakeTaskResult
   private static Application app = new FakeApplication();
 
   public MockFakeTaskResult( Long seqNumber ){
-    this( seqNumber, LocalDateTime.now() );
+    this( seqNumber, LocalDateTime.now());
   }
 
   public MockFakeTaskResult( Long seqNumber, LocalDateTime creationTime ){
-    this( seqNumber, "dontCare" , creationTime, TaskResultType.SUCCESS);
+    this( seqNumber, "dontCare" + seqNumber , creationTime, TaskResultType.SUCCESS);
   }
 
   public MockFakeTaskResult( Long seqNumber, String taskName ){
@@ -27,7 +27,7 @@ public class MockFakeTaskResult
   }
 
   public MockFakeTaskResult( Long seqNumber, TaskResultType taskResultType ){
-    this( seqNumber, "dontCare" , LocalDateTime.now(), taskResultType );
+    this( seqNumber, "dontCare" + seqNumber , LocalDateTime.now(), taskResultType );
   }
 
   public MockFakeTaskResult( Long seqNumber, String taskName, LocalDateTime creationTime ){
@@ -35,7 +35,7 @@ public class MockFakeTaskResult
   }
 
   public MockFakeTaskResult( Long seqNumber, String taskName, LocalDateTime creationTime, TaskResultType taskResultType ){
-    super( app, taskName , null, taskResultType, creationTime, Duration.ofMinutes( (int)Math.random() ));
+    //When we use JsonSerialization we never store millisenconds, so reset it here too
+    super( app, taskName , null, taskResultType, creationTime.withNano(0), Duration.ofMinutes( (int)Math.random() ));
   }
-
 }
