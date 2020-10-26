60ad8ae9a4952ef8ba16b16bc4ec7ea1957738a9
Volodymyr (or Vladimir) Blagodarov <maestrov13@gmail.com>
2020-10-26 11:00:08 +0100

    TaskFiltering: add support for task execution duration filter

--- taskFiltering/src/main/java/filter/TaskResultFilteringSystem.java
+++ taskFiltering/src/main/java/filter/TaskResultFilteringSystem.java
@@ -1,8 +1,11 @@
 package filter;
 
 import filter.single.SingeTaskResultFilterByAppName;
+import filter.single.SingleTaskFilterExecutionDurationAbove;
+import filter.single.SingleTaskFilterExecutionDurationBelow;
 import filter.single.SingleTaskResultFilterByGroupName;
 import filter.single.SingleTaskResultFilterByResultType;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +15,8 @@ import task.TaskResultType;
 import task.criteria.ApplicationNameFilterCriteria;
 import task.criteria.FilterCriteria;
 import task.criteria.FilterCriteriaType;
+import task.criteria.TaskExecutionDurationAboveFilterCriteria;
+import task.criteria.TaskExecutionDurationBelowFilterCriteria;
 import task.criteria.TaskGroupNameFilterCriteria;
 import task.criteria.TaskResultTypeFilterCriteria;
 
@@ -27,12 +32,34 @@ public class TaskResultFilteringSystem {
   @Autowired
   private transient SingleTaskResultFilterByGroupName singleTaskResultFilterByGroupName;
 
+  @Autowired
+  private transient SingleTaskFilterExecutionDurationBelow singleTaskFilterExecutionDurationBelow;
+
+  @Autowired
+  private transient SingleTaskFilterExecutionDurationAbove singleTaskFilterExecutionDurationAbove;
+
   private final transient Collection<FilterChangeListener> filterChangeListeners = new ArrayList<>();
 
   private final Collection<FilterCriteria> filterCriteria = new ArrayList<>();
 
   public void addChangeListener(FilterChangeListener filterChangeListener) {
     filterChangeListeners.add(filterChangeListener);
+  }
+
+  public void updateFilterByExecutionDurationBelow(Duration duration) {
+    singleTaskFilterExecutionDurationBelow.setDurationFilter(duration);
+    removeFilterCriteria(FilterCriteriaType.ExecutionDurationBelow);
+    filterCriteria.add(new TaskExecutionDurationBelowFilterCriteria(duration));
+    filterChangeListeners.stream().forEach(filterChangeListener ->
+        filterChangeListener.onFilterChange(FilterCriteriaType.ExecutionDurationBelow));
+  }
+
+  public void updateFilterByExecutionDurationAbove(Duration duration) {
+    singleTaskFilterExecutionDurationAbove.setDurationFilter(duration);
+    removeFilterCriteria(FilterCriteriaType.ExecutionDurationAbove);
+    filterCriteria.add(new TaskExecutionDurationAboveFilterCriteria(duration));
+    filterChangeListeners.stream().forEach(filterChangeListener ->
+        filterChangeListener.onFilterChange(FilterCriteriaType.ExecutionDurationAbove));
   }
 
   public void updateFilterByTaskGroupName(String taskGroupName) {
@@ -63,6 +90,20 @@ public class TaskResultFilteringSystem {
         filterChangeListener.onFilterChange(FilterCriteriaType.ResultType));
   }
 
+  public void removeTaskExecutionDurationBelowFilter() {
+    singleTaskFilterExecutionDurationBelow.resetFilter();
+    removeFilterCriteria(FilterCriteriaType.ExecutionDurationBelow);
+    filterChangeListeners.stream().forEach(filterChangeListener ->
+        filterChangeListener.onFilterChange(FilterCriteriaType.ExecutionDurationBelow));
+  }
+
+  public void removeTaskExecutionDurationAboveFilter() {
+    singleTaskFilterExecutionDurationAbove.resetFilter();
+    removeFilterCriteria(FilterCriteriaType.ExecutionDurationAbove);
+    filterChangeListeners.stream().forEach(filterChangeListener ->
+        filterChangeListener.onFilterChange(FilterCriteriaType.ExecutionDurationAbove));
+  }
+
   public void removeApplicationNameFilter() {
     singeTaskResultFilterByAppName.resetFilter();
     removeFilterCriteria(FilterCriteriaType.ApplicationName);
@@ -86,7 +127,10 @@ public class TaskResultFilteringSystem {
 
   public boolean isAcceptedByAllFilters(TaskResult taskResult) {
     return singeTaskResultFilterByAppName.isAccepted(taskResult)
-        && singleTaskResultFilterByResultType.isAccepted(taskResult);
+        && singleTaskResultFilterByResultType.isAccepted(taskResult)
+        && singleTaskResultFilterByGroupName.isAccepted(taskResult)
+        && singleTaskFilterExecutionDurationBelow.isAccepted(taskResult)
+        && singleTaskFilterExecutionDurationAbove.isAccepted(taskResult);
   }
 
   private void removeFilterCriteria(FilterCriteriaType criteriaType) {
